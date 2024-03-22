75020e6236196ff06dbd151b4a8c60c8522da2fe
21Z <69188068+21Z@users.noreply.github.com>
2024-03-22 17:12:20 +0400

    Update Constants.js
    Update BASE_IMAGE_URL to point to raw.githubusercontent.com since domain expired

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     BASE_API_URL: "https://pokeapi.co/api/v2",
-    BASE_IMAGE_URL: (id) => `https://pokemon.snowflakedev.org/img/${id}.png`,
+    BASE_IMAGE_URL: (id) => `https://raw.githubusercontent.com/CesiumLabs/pokeres/main/img/${id}.png`,
     IMAGE_MAX: 890,
     IMAGE_MIN: 1,
     POKEMON_MAX: 893
